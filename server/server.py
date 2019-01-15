2fff810fb8af939f67a308a184845a1a0242325f
chluciano <christian.luciano7@gmail.com>
2019-01-15 11:38:48 -0600

    add server.py

--- server/server.py
+++ server/server.py
@@ -1,6 +1,29 @@
-from flask import Flask
+from flask import Flask, jsonify
+from uber_rides.session import Session
+from uber_rides.client import UberRidesClient
 app = Flask(__name__)
 
-@app.route("/", methods=['GET'])
+@app.route("/api/v1/uber", methods=['GET'])
 def hello():
-	return "Hello world"
+	session = Session(server_token="gzsnR5HAFRcEA_q9AVSZX7tEGE1_7ayhiVYwZk3r")
+	client = UberRidesClient(session)
+
+	response = client.get_products(37.77, -122.41)
+	products = response.json.get('products')
+
+	response = client.get_price_estimates(
+	    start_latitude=37.770,
+	    start_longitude=-122.411,
+	    end_latitude=37.791,
+	    end_longitude=-122.405,
+	    seat_count=2
+	)
+
+	estimate = response.json.get('prices')
+	return jsonify(estimate)
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+

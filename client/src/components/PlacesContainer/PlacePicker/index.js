52e5f8df6b3571b2480d01cc24b484b87450f4c2
Bennett Hensey <bennett.hensey@gmail.com>
2019-02-04 17:32:52 -0600

    Fixed button styling bug

--- client/src/components/PlacesContainer/PlacePicker/index.js
+++ client/src/components/PlacesContainer/PlacePicker/index.js
@@ -1,26 +1,26 @@
 import Geosuggest from "react-geosuggest";
 import React, { Component } from "react";
 import "./index.css";
-import Fab from '@material-ui/core/Fab';
-
+import Fab from "@material-ui/core/Fab";
 
 export default class PlacePicker extends Component {
-
-  onClear(){
-    this.props.removeLocation()
-    this._geoSuggest.clear()
+  onClear() {
+    this.props.removeLocation();
+    this._geoSuggest.clear();
   }
 
   render() {
     return (
       <div className="search">
         <Geosuggest
-          ref={el=>this._geoSuggest=el}
+          ref={el => (this._geoSuggest = el)}
           onChange={this.props.removeLocation}
           onSuggestSelect={this.props.setLocation}
           placeholder={this.props.placeholder}
         />
-        <Fab className="clear" onClick={()=>this.onClear()} size ='small'><i class="material-icons">close</i></Fab>
+        <Fab className="clear" onClick={() => this.onClear()} size="small">
+          <i class="material-icons">close</i>
+        </Fab>
       </div>
     );
   }

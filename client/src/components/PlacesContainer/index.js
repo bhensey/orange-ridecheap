1983d37623a8b7288d72da056755f28a5667548b
finleylau <finleylau98@gmail.com>
2019-02-07 12:16:40 -0600

    fixed bug where would load with only one location

--- client/src/components/PlacesContainer/index.js
+++ client/src/components/PlacesContainer/index.js
@@ -10,7 +10,8 @@ export default class PlacesContainer extends Component {
     this.state = { start: null, end: null };
     this.setStart = this.setStart.bind(this);
     this.setEnd = this.setEnd.bind(this);
-
+    this.removeEndLocation = this.removeEndLocation.bind(this);
+    this.removeStartLocation = this.removeStartLocation.bind(this);
     this.callOnComplete = this.callOnComplete.bind(this);
   }
 
@@ -28,11 +29,27 @@ export default class PlacesContainer extends Component {
     }
   }
 
+  removeStartLocation() {
+    this.setState({
+      start: null
+    }, () => {
+      this.props.reset()
+    })
+  }
+
+  removeEndLocation() {
+    this.setState({
+      end: null
+    }, () => {
+      this.props.reset()
+    })
+  }
+
   render() {
     return (
       <div className="placesContainer">
-        <PlacePicker removeLocation={this.props.reset} setLocation={this.setStart} placeholder="Pick-up" />
-        <PlacePicker removeLocation={this.props.reset} setLocation={this.setEnd} placeholder="Drop-off" />
+        <PlacePicker removeLocation={this.removeStartLocation} setLocation={this.setStart} placeholder="Pick-up" />
+        <PlacePicker removeLocation={this.removeEndLocation} setLocation={this.setEnd} placeholder="Drop-off" />
       </div>
     );
   }
